853516d75c52d535b0bb9a47f31ca3bc173eadda
Ridho <ridhoidris@gmail.com>
2019-04-09 20:32:24 +0700

    Menghubungkan Database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Larapi</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -67,7 +67,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                   Siak Api
+                   Larapi
                 </div>
             </div>
         </div>
