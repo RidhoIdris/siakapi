97b87052a5a762cf53b2dc0eeb91b53339a4300a
Ridho <ridhoidris@gmail.com>
2019-04-10 22:40:57 +0700

    create blog

---
+++ app/Http/Controllers/web/BlogController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\web;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('blog.index');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('blog', 'web\BlogController@index')->name('blog.index');
