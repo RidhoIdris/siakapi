cfd74b29da4e4edafe44333d7b0c8afe13944f42
Ridho <ridhoidris@gmail.com>
2019-04-10 21:02:23 +0700

    asd

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Favicon -->
 	<link href="{{ asset('assets/img/favicon.ico') }}" rel="shortcut icon"/>
-
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
 
